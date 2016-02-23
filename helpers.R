da7ce3f938bfa9f7ac268d277e789f993c83b3c0
Andee Kaplan <ajkaplan@iastate.edu>
2016-02-23 10:18:35 -0600

    i think this is on the right track, but not done yet

--- helpers.R
+++ helpers.R
@@ -13,35 +13,58 @@ protoclust_to_list <- function(proto_object) {
   merge <- proto_object$merge %>% data.frame
   names(merge) <- c("node1", "node2")
   merge$height <- proto_object$height
-
   labels <- proto_object$labels
-  nodes <- list()
-  ## http://www.coppelia.io/2014/07/converting-an-r-hclust-object-into-a-d3-js-dendrogram/
-  for (i in (1:nrow(merge))) {
-    
-#     if (merge[i, 1] < 0 & merge[i, 2] < 0) {
-#       nodes[[i]] <- list(name = paste0("node", i), 
-#                          children = list(list(name = labels[-merge[i, 1]]), list(name = labels[-merge[i, 2]])))
-#     } else if (merge[i, 1] > 0 & merge[i, 2] < 0) {
-#       nodes[[i]] <- list(name = paste0("node", i), 
-#                    children = list(list(name = nodes[[merge[i, 1]]]), list(name = labels[-merge[i, 2]])))
-#     } else if (merge[i, 1] < 0 & merge[i, 2] > 0) {
-#       nodes[[i]] <- list(name = paste0("node", i), 
-#                          children = list(list(name = labels[-merge[i, 1]], list(name = nodes[[merge[i, 2]]]))))
-#     } else if (merge[i, 1] > 0 & merge[i, 2] > 0) {
-#       nodes[[i]] <- list(name = paste0("node", i), 
-#                          children = list(list(name = nodes[[merge[i, 1]]], list(name = nodes[[merge[i, 2]]]))))
-#     }
-    
-      
-      if (merge[i,1]<0 & merge[i,2]<0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(list(name=labels[-merge[i,1]]),list(name=labels[-merge[i,2]])))")))}
-      else if (merge[i,1]>0 & merge[i,2]<0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(node", merge[i,1], ", list(name=labels[-merge[i,2]])))")))}
-      else if (merge[i,1]<0 & merge[i,2]>0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(list(name=labels[-merge[i,1]]), node", merge[i,2],"))")))}
-      else if (merge[i,1]>0 & merge[i,2]>0) {eval(parse(text=paste0("node", i, "<-list(name=\"node", i, "\", children=list(node",merge[i,1] , ", node" , merge[i,2]," ))")))}
-   
-  }
-  eval(parse(text=paste0("JSON<-toJSON(node",nrow(merge), ")")))
+  protos <- proto_object$protos
   
-  #return(nodes[[length(nodes)]])
+  ##grow the list items
+  nodes <- merge_to_list_items(merge, labels, protos)
+  
+  ##organize the list items
+  nested_nodes <- nest_items(nodes, list())
 
+}
+
+merge_to_list_items <- function(merge, labels, protos) {
+  elements <- list()
+  for(i in 1:nrow(merge)) {
+    if(merge[i, "node1"] > 0 & merge[i, "node2"] > 0) {
+      #merging two groups
+      elements[[i]] <- list(name = labels[protos[i]], 
+                            height = merge[i, "height"],
+                            children = c(list(name = labels[protos[merge[i, "node1"]]]),
+                                         list(name = labels[protos[merge[i, "node2"]]])))
+    } else if(merge[i, "node1"] < 0 & merge[i, "node2"] > 0) {
+      ##merging one leaf and one group
+      elements[[i]] <- list(name = labels[protos[i]], 
+                            height = merge[i, "height"],
+                            children = c(list(name = labels[-merge[i, "node1"]]),
+                                         list(name = labels[protos[merge[i, "node2"]]])))
+    } else if(merge[i, "node1"] > 0 & merge[i, "node2"] < 0) {
+      ##merging one leaf and one group
+      elements[[i]] <- list(name = labels[protos[i]], 
+                            height = merge[i, "height"],
+                            children = c(list(name = labels[protos[merge[i, "node1"]]]),
+                                         list(name = labels[-merge[i, "node2"]])))
+    } else {
+      ##merging two leafs
+      elements[[i]] <- list(name = labels[protos[i]], 
+                            height = merge[i, "height"],
+                            children = c(list(name = labels[-merge[i, "node1"]]),
+                                         list(name = labels[-merge[i, "node2"]])))
+    }
+  }
+  names(elements) <- labels[protos]
+  return(elements)
+}
+
+nest_items <- function(nodes, nested) {
+  ##need to locate where in nested we should start adding
+  if(nested$name == names(nodes)[length(nodes)])
+  
+  nested <- nodes[[length(nodes)]]
+  children <- nodes[unlist(nodes[[length(nodes)]]$children)]
+  nested$children <- c(children[[1]], 
+                       children[[2]]) #dendrogram, each split has two children
+  
+  remaining <- nodes[!(names(nodes) %in% c(unlist(nodes[[length(nodes)]]$children), nodes[[length(nodes)]]$name))]
 }
