b8710a4239b4d813a448f2d8ed94e2d823c5750b
Andee Kaplan <andee.kaplan@gmail.com>
2021-03-26 16:04:54 -0600

    add all paper data and document

---
+++ R/extdata.R
@@ -0,0 +1,46 @@
+#' Dataset covid19
+#'
+#' This is a protoclust object for use with protoshiny. It is the hierarchical clustering of US counties using mobility data (proportion of people staying home at least 8 hours and proportion of people going into a restaurant).
+#'
+#' @name covid19
+#'
+#' @source \url{https://cmu-delphi.github.io/covidcast/covidcastR/}
+NULL
+
+#' Dataset faces
+#' 
+#' This is a protoclust object for use with protoshiny. It is the hierarchical clustering of 400 grayscale images of faces that correspond to 10 images of 40 individuals.
+#' 
+#' @name faces
+#' 
+#' @source \url{https://www.kaggle.com/kasikrit/att-database-of-faces}
+NULL
+
+#' Dataset movies
+#' 
+#' This is a protoclust object for use with protoshiny. It is the hierarchical clustering of 13,816 movies based on users' ratings and taggings of movies.
+#' 
+#' @name movies
+#' 
+#' @source \url{https://grouplens.org/datasets/movielens/25m/}
+NULL
+
+
+#' Dataset seaflow
+#' 
+#' This is a protoclust object for use with protoshiny. It is the hierarchical clustering of 6,336 scatterplots resulting from continuous-time flow cytometry measurements of phytoplankton.
+#' 
+#' @name seaflow
+#' 
+#' @source Provided by Mattias Rolf Cape and Francois Ribalet.
+NULL
+
+#' Dataset words
+#' 
+#' This is a protoclust object for use with protoshiny. It is the hierarchical clustering of 19,599 of the most common words from Grolier's Encyclopedia.
+#' 
+#' @name words
+#' 
+#' @source \url{https://cs.nyu.edu/~roweis/data.html}
+NULL
+
